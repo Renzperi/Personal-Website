e908883408bd4876da1933c75de014a307b12b74
Renzperi Inandan <inandanrenzperi09@gmail.com>
2023-12-24 10:32:46 +0800

    hide/unhide elements added

--- Personal Website/static/scripts/script.js
+++ Personal Website/static/scripts/script.js
@@ -1,3 +1,15 @@
 const navMenuBtn = document.querySelector("#navMenuBtn");
+const navMenu = document.getElementById("navMenu");
 
-console.log("Hello");
+// functions
+function hideElement(element) {
+  if (element.classList.contains("hide")) {
+    element.classList.remove("hide");
+  } else if (!element.classList.contains("hide")) {
+    element.classList.add("hide");
+  }
+}
+
+navMenuBtn.addEventListener("click", (event) => {
+  hideElement(navMenu);
+});
